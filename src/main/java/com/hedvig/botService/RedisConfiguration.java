e1588fee7b7619dd95ed663b19c31f78839a39fe
Sam Pettersson <sam@sampettersson.com>
2019-05-13 20:07:04 +0200

    Changes according to product-pricing changes

--- src/main/java/com/hedvig/botService/RedisConfiguration.java
+++ src/main/java/com/hedvig/botService/RedisConfiguration.java
@@ -4,17 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
-
-  @Bean
-  JedisConnectionFactory jedisConnectionFactory() {
-    return new JedisConnectionFactory();
-  }
 
   @Bean
   GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer(
@@ -24,7 +19,7 @@ public class RedisConfiguration {
 
   @Bean
   public RedisTemplate<String, Object> redisTemplate(
-    JedisConnectionFactory jedisConnectionFactory,
+    RedisConnectionFactory jedisConnectionFactory,
     GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer) {
     val redisTemplate = new RedisTemplate<String, Object>();
     redisTemplate.setConnectionFactory(jedisConnectionFactory);
