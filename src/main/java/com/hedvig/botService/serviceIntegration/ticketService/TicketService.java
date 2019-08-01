98cf2665a279754e71559a15bcf335c200526b46
fabian.waldner@gmail.com <fabian.waldner@gmail.com>
2019-08-01 15:00:14 +0200

    moved annotation

--- src/main/java/com/hedvig/botService/serviceIntegration/ticketService/TicketService.java
+++ src/main/java/com/hedvig/botService/serviceIntegration/ticketService/TicketService.java
@@ -1,7 +1,9 @@
 package com.hedvig.botService.serviceIntegration.ticketService;
 
 import com.hedvig.botService.serviceIntegration.ticketService.dto.TicketDto;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface TicketService {
 
   void createNewTicket (TicketDto ticket );
--- src/main/java/com/hedvig/botService/serviceIntegration/ticketService/TicketServiceImpl.java
+++ src/main/java/com/hedvig/botService/serviceIntegration/ticketService/TicketServiceImpl.java
@@ -3,10 +3,8 @@ package com.hedvig.botService.serviceIntegration.ticketService;
 import com.hedvig.botService.serviceIntegration.ticketService.dto.TicketDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientResponseException;
 
-@Service
 @Slf4j
 public class TicketServiceImpl implements  TicketService {
   private TicketServiceClient ticketServiceclient;
