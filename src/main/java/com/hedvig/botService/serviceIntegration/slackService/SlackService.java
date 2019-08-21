feef0a86d69f63d8aa7fd41e244f5d1db3dc2be0
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-08-21 14:08:46 +0200

    only send slack messages in production

--- src/main/java/com/hedvig/botService/serviceIntegration/slackService/SlackService.java
+++ src/main/java/com/hedvig/botService/serviceIntegration/slackService/SlackService.java
@@ -5,10 +5,12 @@ import com.hedvig.botService.services.events.OnboardingCallForQuoteEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("production")
 public class SlackService {
   private final SlackServiceClient slackServiceClient;
   private static Logger logger = LoggerFactory.getLogger(SlackService.class);
