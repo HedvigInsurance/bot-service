6aa891aede61da780f091d2803ed0a4043bdf133
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-08-22 16:36:16 +0200

    create new slack profile for slack service

--- src/main/java/com/hedvig/botService/serviceIntegration/slackService/SlackService.java
+++ src/main/java/com/hedvig/botService/serviceIntegration/slackService/SlackService.java
@@ -5,12 +5,13 @@ import com.hedvig.botService.services.events.OnboardingCallForQuoteEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("production")
+@Profile("slack")
 public class SlackService {
   private final SlackServiceClient slackServiceClient;
   private static Logger logger = LoggerFactory.getLogger(SlackService.class);
