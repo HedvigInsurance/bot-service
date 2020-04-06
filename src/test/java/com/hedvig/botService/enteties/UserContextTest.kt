e40dd78a1e0295c53ee46371b01793a6284a5178
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-04-06 13:18:00 +0200

    start free text chat if in norway

--- src/test/java/com/hedvig/botService/enteties/UserContextTest.kt
+++ src/test/java/com/hedvig/botService/enteties/UserContextTest.kt
@@ -4,6 +4,7 @@ import com.hedvig.botService.testHelpers.TestData.TOLVANSSON_MEMBER_ID
 
 import com.hedvig.botService.chat.Conversation
 import com.hedvig.botService.chat.ConversationFactory
+import com.hedvig.botService.chat.FreeChatConversation
 import com.hedvig.botService.chat.OnboardingConversationDevi
 import com.hedvig.botService.services.SessionManager.Intent
 import org.junit.Test
@@ -11,6 +12,7 @@ import org.junit.runner.RunWith
 import org.mockito.BDDMockito.*
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
 
 @RunWith(MockitoJUnitRunner::class)
 class UserContextTest {
@@ -33,5 +35,21 @@ class UserContextTest {
         then<Conversation>(mockConversation)
             .should(times(1))
             .init(OnboardingConversationDevi.MESSAGE_ONBOARDINGSTART_ASK_NAME)
+    }
+
+    @Test
+    fun getMessages_initFreeChat_callsInitWithCorrectStartMessage() {
+
+        val uc = UserContext(TOLVANSSON_MEMBER_ID)
+        uc.locale = Locale("en", "NO")
+
+        given(conversationFactory!!.createConversation(FreeChatConversation::class.java, uc))
+            .willReturn(mockConversation)
+
+        uc.getMessages(Intent.ONBOARDING, conversationFactory)
+
+        then<Conversation>(mockConversation)
+            .should(times(1))
+            .init(FreeChatConversation.FREE_CHAT_ONBOARDING_START)
     }
 }
